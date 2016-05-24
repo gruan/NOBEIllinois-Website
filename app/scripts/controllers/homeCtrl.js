5f88b62f8c1a9587b26dc1485df8f5bd032a68a1
george <ruan3@illinois.edu>
2016-05-23 19:42:22 -0700

    Skeleton for home controller

---
+++ app/scripts/controllers/homeCtrl.js
@@ -0,0 +1,18 @@
+/**
+ * Created by George Ruan on May 23, 2016.
+ *
+ * HomeCtrl defines the behavior of the Home Page.
+ */
+
+(function() {
+    'use strict';
+
+    angular.module('nobe')
+      .controller('HomeCtrl', homeCtrl);
+
+      homeCtrl.$inject = ['$scope'];
+
+      function homeCtrl($scope) {
+
+      }
+})();
