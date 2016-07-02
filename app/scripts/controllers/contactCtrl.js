fe6452a435dd499ade4cd7ea68f032e61fb89d4e
george <ruan3@illinois.edu>
2016-07-02 14:40:49 -0700

    Fix url on navbar

---
+++ app/scripts/controllers/contactCtrl.js
@@ -0,0 +1,18 @@
+/**
+ * Created by George Ruan on June 28, 2016.
+ *
+ * ContactCtrl defines the behavior of the Contact Page.
+ */
+
+(function() {
+  'use strict';
+
+  angular.module('nobe')
+    .controller('ContactCtrl', contactCtrl);
+
+  contactCtrl.$inject = ['$scope'];
+
+  function contactCtrl($scope) {
+  }
+
+})();
