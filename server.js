35ea99f4b20be7ef69b9ed1c14e4b1745c6dd3f2
george <ruan3@illinois.edu>
2016-07-24 03:22:19 -0700

    Reroute to node_modules and get rid of fluff

--- server.js
+++ server.js
@@ -2,15 +2,24 @@
   'use strict';
   var express = require('express');
   var app = express();
-  var fs = require('fs');
 
   app.use(express.static('app'));
 
   // ===================
   // Node_Modules Routes
   // ===================
-  app.use('/angular-chart', express.static(__dirname + '/node_modules/angular-chart.js/dist'));
-  app.use('/chart', express.static(__dirname + '/node_modules/chart.js/dist'));
+
+  app.use('/scripts/angular', express.static(__dirname + '/node_modules/angular/'));
+  app.use('/scripts/angular-chart', express.static(__dirname + '/node_modules/angular-chart.js/dist/'));
+  app.use('/scripts/angular-route', express.static(__dirname + '/node_modules/angular-route/'));
+  app.use('/scripts/chart', express.static(__dirname + '/node_modules/chart.js/dist/'));
+  app.use('/scripts/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+  app.use('/scripts/lodash', express.static(__dirname + '/node_modules/lodash'));
+  app.use('/scripts/wow', express.static(__dirname + '/node_modules/wowjs/dist'));
+
+  app.use('/styles/animate', express.static(__dirname + '/node_modules/wowjs/css/libs'));
+  app.use('/styles/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+  app.use('/styles/font-awesome', express.static(__dirname + '/node_modules/font-awesome/'));
 
   app.get('/', function (req, res) {
     res.sendFile(__dirname + '/app/index.html');
